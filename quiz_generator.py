ae2d9fcb3e6b5c110181f8cbfe5f91afe2f0a93c
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-11 08:20:36 +0800

    Asks the user for the quiz questions, added condition to exit the program

--- quiz_generator.py
+++ quiz_generator.py
@@ -4,7 +4,12 @@ def quiz_creator():
     filename = "quiz_questions.txt"
     #overwrite content in file and assign the file object as file
     with open(filename, "w") as file:
-#loop the program until user wants to exit
+        #loop the program until user wants to exit
+        while True:
+            questions = input("Enter the quiz question (Press 'e' to exit): ")
+            if questions.lower() == 'e':
+                print("Exiting the program.")
+                break
 #ask the user for quiz questions
 #ask for the choices
 #ask for the correct answer
