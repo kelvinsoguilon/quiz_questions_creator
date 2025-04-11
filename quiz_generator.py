d89b405240a6f6044d78b2b3a9249ea30ea83892
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-11 08:16:58 +0800

    Overwrites the content on the file and assign the file object as file to write to the file

--- quiz_generator.py
+++ quiz_generator.py
@@ -2,6 +2,8 @@
 def quiz_creator():
     #create a file path or directory for the txt file
     filename = "quiz_questions.txt"
+    #overwrite content in file and assign the file object as file
+    with open(filename, "w") as file:
 #loop the program until user wants to exit
 #ask the user for quiz questions
 #ask for the choices
