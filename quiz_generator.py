b9726aba29dba26053072c452397ce55ebf392a4
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-04-11 08:02:34 +0800

    Added pseudocode to help code the program more efficiently

---
+++ quiz_generator.py
@@ -0,0 +1,7 @@
+#define quiz creator program
+#create a file path or directory for the txt file
+#loop the program until user wants to exit
+#ask the user for quiz questions
+#ask for the choices
+#ask for the correct answer
+#save all the data to the txt file
