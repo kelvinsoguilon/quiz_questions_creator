521f67fd07848d75ec47dfc9ccbbd925123304fe
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-05-05 22:41:02 +0800

    Added questions and answers variable to store the contents of the file

--- quiz_taker.py
+++ quiz_taker.py
@@ -2,9 +2,12 @@
 def load_questions_with_answers(filepath):
     with open(filepath, 'r') as file:
         lines = file.readlines() 
-#determine how the questions and answers are stored in the txt file
+
+    #store the questions and answers
+    questions = []
+    answers = {}
+    #determine how the questions and answers are stored in the txt file
 #parse the questions
 #parse the answers
-#store the questions and answers
 #randomly select the questions
 #score counter
