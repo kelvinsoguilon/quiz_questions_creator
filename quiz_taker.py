5b4aea1c8f08d3702890a3d7db2cc4412e60e637
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-05-05 22:22:49 +0800

    Added steps to code the program more efficiently

---
+++ quiz_taker.py
@@ -0,0 +1,7 @@
+#determine how the questions and answers are stored in the txt file
+#select file to load and read it
+#parse the questions
+#parse the answers
+#store the questions and answers
+#randomly select the questions
+#score counter
