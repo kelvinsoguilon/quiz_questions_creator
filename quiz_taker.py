05d9a3afcdb7a9b6d40c0481469c02727c58fce3
kelvinsoguilon <soguilonkelvin@gmail.com>
2025-05-05 22:37:04 +0800

    Select the file to open and read the contents

--- quiz_taker.py
+++ quiz_taker.py
@@ -1,5 +1,8 @@
-#determine how the questions and answers are stored in the txt file
 #select file to load and read it
+def load_questions_with_answers(filepath):
+    with open(filepath, 'r') as file:
+        lines = file.readlines() 
+#determine how the questions and answers are stored in the txt file
 #parse the questions
 #parse the answers
 #store the questions and answers
